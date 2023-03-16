e7cb681607a2b04657463f92ad8e34dbe768b3fc
lsulsulsu <126743189+TTTWIXX@users.noreply.github.com>
2023-03-17 08:46:04 +0900

    0317 0846

--- 1. 자바스크립트 기초/쿵쿵따.js
+++ 1. 자바스크립트 기초/쿵쿵따.js
@@ -1,23 +1,48 @@
-alert(`끝말잇기 게임
-========================
-조건1. 단어는 3글자로 한정한다.
-조건2. 3초이내에 답하지 못하면 패배한다.
-조건3. 중복된 단어를 입력할경우 패배한다.
-조건4. 이전 단어의 끝말과 현재 단어의 첫 단어가 일치해야한다.
-조건5. 숫자는 입력할수 없다.`)
+// alert(`끝말잇기 게임
+// ========================
+// 조건1. 단어는 3글자로 한정한다.
+// 조건2. 3초이내에 답하지 못하면 패배한다.
+// 조건3. 중복된 단어를 입력할경우 패배한다.
+// 조건4. 이전 단어의 끝말과 현재 단어의 첫 단어가 일치해야한다.
+// 조건5. 숫자는 입력할수 없다.`)
 
 
-while(true){
-    alert('쿵쿵따리 쿵쿵따 쿵쿵따리 쿵쿵따 쿵쿵쿵쿵');
-    var firstWord=prompt(`쿵쿵따`);
-    var secondWord=prompt('쿵쿵따');
-    if(firstWord.charAt(firstWord.length-1)===secondWord.charAt(0)){
-        alert(`${secondWord} 쿵쿵따`);
-    }else{
-        alert('땡!!!!!!!!!!');
-    }
+// while(true){
+//     alert('쿵쿵따리 쿵쿵따 쿵쿵따리 쿵쿵따 쿵쿵쿵쿵');
+//     // var firstWord=prompt();
+    // var secondWord=prompt();
+    // if(firstWord.charAt(firstWord.length-1)===secondWord.charAt(0)){
+    //     alert(`${secondWord} 쿵쿵따`);
+    // }else{
+    //     alert('땡!!!!!!!!!!');
+    // }
+
+
+
     
-}
+
+    // for (var i = 0; i < 4; i++) {
+    //     var word = prompt()
+    //     eval("goong" + i + "=" + i + "+word");
+    //     if(goong`+i+`===goong`+i+1+`){
+    //         alert('참')
+
+
+    //     }
+
+
+
+    // }
+    // alert(goong0)
+    // alert(goong1)
+    // alert(goong2)
+    // alert(goong3)
+
+      
+    // }
+    
+    
+
 //     var count=0;
 //     count++
 //     if(
