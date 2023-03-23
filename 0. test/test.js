3255f8d627951885037e0ff6bb08a2d16e1e543b
lsulsulsu <126743189+TTTWIXX@users.noreply.github.com>
2023-03-23 17:55:13 +0900

    0323 17:55

--- 0. test/test.js
+++ 0. test/test.js
@@ -1,9 +1,14 @@
-const [obj]=[{id:1234, pw:5678}, function sum (a,b){
-    return a+b;
-}]
 
-
-const [filterData]=[obj].filter(u=>u.id===1234)
-
-
-console.log(filterData);
+function solution(numbers) {
+    let sum=0; 
+    var answer; 
+    for(let i=0;i<numbers.length;i++){
+        sum+=numbers[i];
+    }
+    answer=sum/numbers.length;
+    if( answer-Math.floor(answer)===0 || answer-Math.floor(answer)===0.5){
+    return answer;
+    }
+}
+const numbers=[89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
+console.log(solution(numbers));
