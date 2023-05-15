b03b70764d47dad462ea71b47645c7f38c06bd0e
TTTWIXX <tkddnr8181@gmail.com>
2023-05-15 21:29:28 +0900

    1

--- 0. test/test.js
+++
@@ -1,8 +0,0 @@
-
-const userNames= [`김철수`,`강감찬`,`박영희`];
-const [a,b,c]=userNames;
-console.log(a,b,c);
-const [firstObj] = [{id:1},{id:2},{id:3}].filter(obj=>obj.id>=2);
-console.log(firstObj);
-const copynames=[...userNames];
-console.log(copynames);
